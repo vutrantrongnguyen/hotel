fa2088d3934d73ddccb43daa0c706c58a26aeb77
nguyenvtt <quenmatkhaupart3@gmail.com>
2019-12-24 14:01:21 +0700

    complete hotel code

--- resources/views/admin/room/edit.blade.php
+++ resources/views/admin/room/edit.blade.php
@@ -27,11 +27,30 @@
                 </div>
                 <div class="form-group">
                     <label for="description">Mô tả</label>
-                    <textarea class="form-control" id="description" placeholder="..." name="description">
-                        {{$room->description}}
-                    </textarea>
-
+{{--                    <textarea class="form-control" id="description" placeholder="..." name="description">--}}
+{{--                        {{$room->description}}--}}
+{{--                    </textarea>--}}
+                    <textarea class="form-control" name="description" id="editor" required placeholder="Nhập phần nội dung bài viết">
+                       {{$room->description}}
+                        </textarea>
                 </div>
+                <script>
+                    ClassicEditor
+                        .create(document.querySelector('#description'))
+                        .catch(error => {
+                            console.error(error);
+                        });
+                </script>
+                <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+                <script> CKEDITOR.replace('editor', {
+                        filebrowserBrowseUrl: '{{ asset('ckfinder/ckfinder.html') }}',
+                        filebrowserImageBrowseUrl: '{{ asset('ckfinder/ckfinder.html?type=Images') }}',
+                        filebrowserFlashBrowseUrl: '{{ asset('ckfinder/ckfinder.html?type=Flash') }}',
+                        filebrowserUploadUrl: '{{ asset('ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files') }}',
+                        filebrowserImageUploadUrl: '{{ asset('ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images') }}',
+                        filebrowserFlashUploadUrl: '{{ asset('ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash') }}'
+                    } );
+                </script>
 
                 <div class="form-group">
                     <label for="price">Giá</label>
